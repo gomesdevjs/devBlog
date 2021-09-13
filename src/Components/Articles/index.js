b1de2d01ccdff80bfd3f674b8d2cdb43d1fe1a44
gomesdevjs <gomes.dev.js@gmail.com>
2021-09-13 08:58:14 -0300

    Stylization of posts

--- src/Components/Articles/index.js
+++ src/Components/Articles/index.js
@@ -4,11 +4,20 @@ import Posts from "../Posts";
 // import { Container } from './styles';
 
 function Articles() {
+  // Request data to API
+
   return (
-    <>
+    <section className="container border">
       <h1>Article</h1>
-      <Posts />
-    </>
+      <div className="containerPosts">
+        <Posts subtitle="tecnologia" title="O guia definititivo sobre o blog" />
+        <Posts
+          subtitle="fotografia"
+          title="Quais as melhores cameras para 2022?"
+        />
+        <Posts subtitle="cinema" title="Os 10 filmes com maior bilheteria" />
+      </div>
+    </section>
   );
 }
 
--- src/Components/Posts/index.js
+++ src/Components/Posts/index.js
@@ -1,5 +1,24 @@
-const Posts = () => {
-  return <h1>Posts</h1>;
+const Posts = ({ title, subtitle }) => {
+  return (
+    <div className="post border">
+      <div className="imgPost"></div>
+      <h5>{subtitle}</h5>
+      <h3>{title}</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+        perferendis pariatur autem deleniti eos inventore quidem a veritatis
+        quos nihil sunt ea explicabo exercitationem aspernatur unde quam,
+        voluptate dignissimos quae.
+      </p>
+      <div className="flex pt-2">
+        <div className="imgProfile"></div>
+        <div className="descriptionProfile ml-2">
+          <h6 className="color-blue">Fulano de tal</h6>
+          <p>September, 13 - 5 min ago</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
