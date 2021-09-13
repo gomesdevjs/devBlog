8fb293dcacf97407cf19685d803610ade9a475c0
gomesdevjs <gomes.dev.js@gmail.com>
2021-09-13 02:29:38 -0300

    Add a new menu in Dev Blog

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,38 @@
 import React from "react";
+import Logo from "../../images/logo.png";
+const Header = () => {
+  // Javascript
+  const descLogo = "This is Blog Logo";
 
-// import { Container } from './styles';
-
-function Header() {
-  return <h1>Header.js</h1>;
-}
+  return (
+    <>
+      <header className="flex-space-between">
+        <div className="">
+          <img src={Logo} alt={descLogo} />
+        </div>
+        <div className="search">
+          <input className="inputSearch" type="text" name="search" />
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#" className="navLink">
+              Categories
+            </a>
+          </li>
+          <li>
+            <a href="#" className="navLink">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="navLink">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
 
 export default Header;
